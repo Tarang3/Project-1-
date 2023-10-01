a09dad8c48601640f4744c2126279455d69f941e
Tarang3 <146675127+Tarang3@users.noreply.github.com>
2023-10-01 16:13:40 -0600

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -14,25 +14,32 @@ int main() {
     const int UPPER_BOUND = 200;
 
     // Enumeration for menu choices
-    enum options { DisplayLeft = 1, DisplayRight = 2, Guess = 3, Change = 4, Exit = 5};
+    enum options { DisplayLeft = 1, DisplayRight = 2, Guess = 3, Change = 4, Exit = 5 };
 
-    // Variable initialization
+    // Initialize variables
+    string userName;
     int userScore = INITIAL_SCORE;
     int pointGain = POINTS_GAIN_INITIAL;
     int pointLoss = POINTS_LOSS_INITIAL;
     int lowerBound = -1;
     int upperBound = -1;
-    string userName;
+    int userGuess;
     bool gameOver = false;
+
+    srand(time(0));
+    upperBound = rand() % 101 + 100;
+    do {
+        lowerBound = rand() % 101 + 100;
+    } while (upperBound <= lowerBound);
 
     // Display student information
     cout << "+-------------------------------------------------+" << endl;
-    cout << "| Computer Science and Engineering                |" << endl;
+    cout << "| Computer Science and Engineering                 |" << endl;
     cout << "| CSCE 1030 - Computer Science I                  |" << endl;
     cout << "| Student Name EUID euid@my.unt.edu               |" << endl;
     cout << "+-------------------------------------------------+" << endl;
 
-    // Get and validate user's name
+    // invalid and valid name
     while (true) {
         cout << "Enter your name: ";
         getline(cin, userName);
@@ -53,16 +60,17 @@ int main() {
             }
             break;
         } else {
-            cout << "Invalid name format. Please enter a valid name." << endl;
+            cout << "Error name format is invalid. Please enter a valid name." << endl;
         }
     }
 
-    // Game loop
+    // While loop
     while (!gameOver && userScore >= 0) {
         // Display lower and upper bounds
         cout << lowerBound << "     " << upperBound << endl;
+    }
 
-        // Display menu
+        // What to display
         cout << "1. Display Left Number" << endl;
         cout << "2. Display Right Number" << endl;
         cout << "3. Guess a number in between" << endl;
@@ -73,13 +81,13 @@ int main() {
         cin >> userChoice;
 
         // Process user choice
-        switch (static_cast<MenuChoice>(userChoice)) {
+        switch (static_cast<options>(userChoice)) {
             case DisplayLeft:
                 if (lowerBound == -1) {
                     lowerBound = LOWER_BOUND;
                     pointGain = 1;
                     pointLoss = 10;
-                    cout << "You will only get 1 point for guessing correctly and lose 10 points for guessing incorrectly, now." << endl;
+                    cout << "Now, you will gain " << pointGain << " points for guessing correctly and lose " << pointLoss << " for guessing incorrectly." << endl;
                 } else {
                     cout << "You have already displayed the left boundary, you cannot display both." << endl;
                 }
@@ -90,57 +98,44 @@ int main() {
                     upperBound = UPPER_BOUND;
                     pointGain = 1;
                     pointLoss = 10;
-                    cout << "You will only get 1 point for guessing correctly and lose 10 points for guessing incorrectly, now." << endl;
+                    cout << "Now, you will gain " << pointGain << " points for guessing correctly and lose " << pointLoss << " for guessing incorrectly." << endl;
                 } else {
                     cout << "You have already displayed the right boundary, you cannot display both." << endl;
                 }
                 break;
 
             case Guess:
-                int userGuess;
                 cout << "Enter your guess: ";
                 cin >> userGuess;
                 if (userGuess > lowerBound && userGuess < upperBound) {
                     userScore += pointGain;
-                    cout << "You guessed correctly. You get " << pointGain << " points." << endl;
-                    cout << "Your remaining points=" << userScore << endl;
+                    cout << "You guessed correctly. You gain " << pointGain << " points." << endl;
+                    cout << "Your remaining points = " << userScore << "." << endl;
                 } else {
                     userScore -= pointLoss;
                     cout << "You guessed incorrectly. You lose " << pointLoss << " points." << endl;
-                    cout << "Your remaining points=" << userScore << endl;
+                    cout << "Your remaining points = " << userScore << "." << endl;
                 }
                 break;
 
             case Change:
-                srand(time(0));
-                do {
-                    lowerBound = rand() % (UPPER_BOUND - LOWER_BOUND + 1) + LOWER_BOUND;
-                    upperBound = rand() % (UPPER_BOUND - LOWER_BOUND + 1) + LOWER_BOUND;
-                } while (lowerBound >= upperBound);
+                cout << "New range has been generated." << endl;
                 pointGain = POINTS_GAIN_INITIAL;
                 pointLoss = POINTS_LOSS_INITIAL;
+                upperBound = rand() % 101 + 100;
+                do {
+                    lowerBound = rand() % 101 + 100;
+                } while (upperBound <= lowerBound);
+                lowerBound = -1;
+                upperBound = -1;
                 userScore--;
-                cout << "New range has been generated." << endl;
-                cout << "Points deducted for changing numbers. Your updated points balance: " << userScore << endl;
                 break;
 
             case Exit:
                 gameOver = true;
                 cout << "Bye " << userName << " !!!" << endl;
                 break;
-
-            default:
-                cout << "Invalid choice. Please select a valid option." << endl;
-                break;
         }
-    }
-
-    // Display final results using a while loop
-    if (userScore < 0) {
-        cout << "You are out of points." << endl;
-    } else {
-        cout << "Your final points balance: " << userScore << endl;
-    }
-
-    return 0;
 }
+                
+    return 0;
